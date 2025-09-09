f4708718c1410b6d48c08733f5ac5b04ca513227
Aline Fernandes <fernandes.dev@outlook.com>
2025-09-09 14:08:46 -0300

    if & else

---
+++ aula-3-if-else/aula3.js
@@ -0,0 +1,25 @@
+/*
+
+If & Else -> Controlador de fluxo
+
+If(SE)
+Else(SE NÃO)
+
+Operadores de Comparação
+> maior que
+< menor que
+== igual que
+=== igual que
+>= maior ou igual que
+<= menor ou igual que
+
+*/
+
+const notaDoAluno = 5
+
+if(notaDoAluno > 5){
+    console.log("Aluno APROVADO")
+}
+else {
+    console.log("Aluno REPROVADO")
+}
